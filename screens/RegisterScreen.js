d800b864802277da5029e5dfdf029662da23f030
encepsuryana <encep.suryanajr@gmail.com>
2022-02-11 15:26:55 +0700

    Update Register

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -63,12 +63,14 @@ const RegisterScreen = () => {
           }}
         >
           <View style={styles.inputContainer}>
-            <TextInput
+            {/* disable   
+          <TextInput
               placeholder="Username"
               value={username}
               onChangeText={(text) => setUsername(text)}
               style={styles.input}
             />
+           */}
             <TextInput
               placeholder="Email"
               value={email}
@@ -110,50 +112,3 @@ const RegisterScreen = () => {
 };
 
 export default RegisterScreen;
-
-const styles1 = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  inputContainer: {
-    width: "80%",
-  },
-  input: {
-    backgroundColor: "white",
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderRadius: 10,
-    marginTop: 5,
-  },
-  buttonContainer: {
-    width: "60%",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 40,
-  },
-  button: {
-    backgroundColor: "#0782f9",
-    width: "100%",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
-  },
-  buttonOutline: {
-    backgroundColor: "white",
-    marginTop: 5,
-    borderColor: "#0782f9",
-    borderWidth: 2,
-  },
-  buttonOutlineText: {
-    color: "#0782f9",
-    fontSize: 16,
-    fontWeight: "700",
-  },
-});
